d2dde3a802785d06b8a349746cf3d6c8052eecc1
prem-minister <premjr08@gmail.com>
2022-07-17 04:20:11 +0800

    deploy weather app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ebecd5bd31msh5b5fd7b3cbdc703p1c3c7cjsn86efbe1099e7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "92f67a0ed9dea38cf4be54a417b77fe6";
